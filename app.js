b772afb72fc65d19c30b56f50aeb692b326de425
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-12-22 05:46:23 +0200

    making all project files

---
+++ app.js
@@ -0,0 +1,40 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/secretsDB", {useNewUrlParser: true});
+const userSchema = {
+    name : String,
+    password: String
+};
+const User = mongoose.model("User", userSchema);
+  
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+})
+
+app.post("/register",function(req,res){
+    
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+
