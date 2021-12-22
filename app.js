61fe3f1d9aa0aa7c7bdd38c444f6209d207f14a7
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-12-22 20:03:05 +0200

    adding level 2 security layer

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const mongooseEncryption = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,18 +13,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/secretsDB", {useNewUrlParser: true});
-const userSchema = {
+const userSchema = new mongoose.Schema({
     name : String,
     password: String
-};
+});
+
+
+userSchema.plugin(mongooseEncryption,{secret:process.env.SECRET , encryptedFields:["password"]});
+
 const User = mongoose.model("User", userSchema);
   
 app.get("/",function(req,res){
     res.render("home");
-});
-
-app.get("/login",function(req,res){
-    res.render("login");
 });
 
 app.get("/register",function(req,res){
@@ -31,8 +32,38 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req,res){
-    
+    const newUser = new User ({
+        name : req.body.username,
+        password : req.body.password
+    });
+    newUser.save(function(err){
+        if(err){
+            res.send(err);
+        }else{
+            res.render("secrets");
+        }
+    })
 })
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/login",function(req,res){
+    const userName = req.body.username;
+    const password =req.body.password;
+    User.findOne({name:userName},function(err,user){
+        if (err){
+            res.send(err);
+        }else{
+            if (user){
+                if (user.password === password ) {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
